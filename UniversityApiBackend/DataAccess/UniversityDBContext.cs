5f64ffde132564949946721b8ac41dfe24aff0f7
Anastasia Ramos <anastasia.ramosmayor@gmail.com>
2022-11-03 13:38:23 +0000

    Ejercicio completo con clase Curso
    Ejercicio completo con clase Curso

--- UniversityApiBackend/DataAccess/UniversityDBContext.cs
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -10,5 +10,6 @@ namespace UniversityApiBackend.DataAccess
 
         //TODO: Add DbSets (Tables of our DataBase)
         public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
     }
 }
---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public enum Nivel { Basico, Intermedio, Avanzado }
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        public string? DescripcionLarga { get; set; }
+        public string? PublicoObjetivo { get; set; }
+        public string? Objetivos { get; set; }
+        public string? Requisitos { get; set; }
+        [Required]
+        public Nivel Nivel { get; set; } = Nivel.Basico;
+    }
+}
